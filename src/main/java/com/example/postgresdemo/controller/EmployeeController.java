3bbf57db63b75d025372d174e99a39fb4400a638
junior <junior670@hotmail.com>
2021-01-11 23:46:53 -0500

    Segundo commit je.

--- src/main/java/com/example/postgresdemo/controller/EmployeeController.java
+++ src/main/java/com/example/postgresdemo/controller/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.example.postgresdemo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +40,28 @@ public class EmployeeController {
 		return ResponseEntity.ok().body(employee);
 		
 	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id")
+	Long employeeId, @RequestBody Employee employeeDetails) throws ResourceNotFoundException{
+		Employee employee = employeeRepository.findById(employeeId)
+		.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		
+		employee.setEmailId(employeeDetails.getEmailId());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setFirstName(employeeDetails.getFirstName());
+		final Employee updateEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updateEmployee);
+	}
+	
+	
+	/*employeeRepository.delete(employee);
+	Map<String, Boolean> response = new HashMap<>();
+	response.put("delete", Boolean.TRUE);*/
 
 }
