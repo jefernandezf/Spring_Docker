1194d757f0a771b2da86a6754e303d5548008e59
junior <junior670@hotmail.com>
2021-01-11 20:32:08 -0500

    Primer Commit

---
+++ src/main/java/com/example/postgresdemo/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.example.postgresdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.postgresdemo.exception.ResourceNotFoundException;
+import com.example.postgresdemo.model.Employee;
+import com.example.postgresdemo.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId,
+			@Validated @RequestBody Employee employeeDetails) throws ResourceNotFoundException{
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		return ResponseEntity.ok().body(employee);
+		
+	}
+
+}
---
+++ src/main/java/com/example/postgresdemo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.example.postgresdemo.exception;
+
+public class GlobalExceptionHandler {
+
+}
